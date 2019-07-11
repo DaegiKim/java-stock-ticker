489a19333fe0dbdc9eaf40d6b59b20e7c007b303
DaegiKim <rmrhtdms@gmail.com>
2019-07-11 21:17:54 +0900

    minor tweaks

--- src/kim/daegi/Main.java
+++ src/kim/daegi/Main.java
@@ -13,16 +13,14 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static final String API_ENDPOINT="https://query1.finance.yahoo.com/v7/finance/quote?lang=ko-KR&region=KR&corsDomain=finance.yahoo.com";
+    private static final String API_ENDPOINT="https://query1.finance.yahoo.com/v7/finance/quote?lang=ko-KR&region=KR&corsDomain=finance.yahoo.com";
 
     public static void main(String[] args) {
-
-        String interval = args[0];
+        long interval = Long.parseLong(args[0]);
         String symbols = Arrays.stream(args).skip(1).collect(Collectors.joining(","));
 
         try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
@@ -30,13 +28,10 @@ public class Main {
 
             while (true) {
                 HttpResponse result = httpClient.execute(request);
-                String json = EntityUtils.toString(result.getEntity(), "UTF-8");
-
-                JSONObject jsonObject = new JSONObject(json);
-
-                print(jsonObject);
+                String json = EntityUtils.toString(result.getEntity());
+                print(new JSONObject(json));
                 try {
-                    Thread.sleep(Long.parseLong(interval));
+                    Thread.sleep(interval);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -47,46 +42,44 @@ public class Main {
     }
 
     private static void print(JSONObject jsonObject) {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-
         JSONArray result = jsonObject.getJSONObject("quoteResponse").getJSONArray("result");
 //        result = sort(result);
 
-        System.out.printf(ConsoleColors.CYAN_UNDERLINED+"%-15s%-10s%20s%10s%10s%12s %-20s\033[0m\n", "Name", "Symbol", "Low-High", "Price", "Diff", "Percent", "longName");
-
+        StringBuilder sb = new StringBuilder();
+        sb.append("\033[H\033[2J");
+        sb.append(String.format(ConsoleColors.CYAN_UNDERLINED+"%-15s%10s%10s%11s%10s %-20s\033[0m\n", "Name", "Symbol", "Price", "Diff", "Percent", "Long Name"));
 
         for (int i = 0; i < result.length(); i++) {
             JSONObject data = result.getJSONObject(i);
 
-            String shortName = data.getString("shortName");
-            String longName = shortName;
-            if(data.has("longName")) {
-                longName = data.getString("longName");
-            }
+            String shortName = data.optString("shortName");
             shortName = shortName.length()>14?shortName.substring(0, 14):shortName;
-            String symbol = data.getString("symbol");
-            String regularMarketDayRange = data.getString("regularMarketDayRange");
-            double regularMarketPrice = data.getDouble("regularMarketPrice");
-            double regularMarketDayHigh = data.getDouble("regularMarketDayHigh");
-            double regularMarketDayLow = data.getDouble("regularMarketDayLow");
-            double regularMarketChange = data.getDouble("regularMarketChange");
-            double regularMarketChangePercent = data.getDouble("regularMarketChangePercent");
+
+            String longName = data.optString("longName", shortName);
+            String symbol = data.optString("symbol");
+
+            double regularMarketPrice = data.optDouble("regularMarketPrice");
+            double regularMarketDayHigh = data.optDouble("regularMarketDayHigh");
+            double regularMarketDayLow = data.optDouble("regularMarketDayLow");
+            double regularMarketChange = data.optDouble("regularMarketChange");
+            double regularMarketChangePercent = data.optDouble("regularMarketChangePercent");
 
             String color = regularMarketChange==0?"":regularMarketChange>0?ConsoleColors.GREEN_BOLD_BRIGHT:ConsoleColors.RED_BOLD_BRIGHT;
 
-            System.out.printf("%-15s", shortName);
-            System.out.printf("%-10s", symbol);
-            System.out.printf("%20s", regularMarketDayRange);
+            sb.append(String.format("%-15s", shortName));
+            sb.append(String.format("%10s", symbol));
+
             if(regularMarketDayHigh == regularMarketPrice || regularMarketDayLow == regularMarketPrice) {
-                System.out.printf(ConsoleColors.WHITE_BOLD+color+"%10.2f"+ConsoleColors.RESET, regularMarketPrice);
+                sb.append(String.format(ConsoleColors.WHITE_BOLD+color+"%10.2f"+ConsoleColors.RESET, regularMarketPrice));
             } else {
-                System.out.printf(ConsoleColors.WHITE_BOLD+"%10.2f"+ConsoleColors.RESET, regularMarketPrice);
+                sb.append(String.format(ConsoleColors.WHITE_BOLD+"%10.2f"+ConsoleColors.RESET, regularMarketPrice));
             }
-            System.out.printf(color+"%10.2f"+ConsoleColors.RESET, regularMarketChange);
-            System.out.printf(color+"%12s"+ConsoleColors.RESET, String.format("(%.2f%%)", regularMarketChangePercent));
-            System.out.printf(" %-20s\n", longName);
+
+            sb.append(String.format(color+"%11s"+ConsoleColors.RESET, String.format("%.2f", regularMarketChange)+" "+(regularMarketChange>0?"▲":regularMarketChange<0?"▼":"-")));
+            sb.append(String.format(color+"%10s"+ConsoleColors.RESET, String.format("(%.2f%%)", regularMarketChangePercent)));
+            sb.append(String.format(" %-20s\n", longName));
         }
+        System.out.print(sb);
     }
 
     private static JSONArray sort(JSONArray result) {
@@ -97,18 +90,16 @@ public class Main {
             jsonValues.add(result.getJSONObject(i));
         }
 
-        Collections.sort( jsonValues, (a, b) -> {
+        jsonValues.sort((a, b) -> {
             double valA = a.getDouble("regularMarketChangePercent");
             double valB = b.getDouble("regularMarketChangePercent");
-
-            if (valA < valB) return 1;
-            if (valA > valB) return -1;
-            return 0;
+            return Double.compare(valB, valA);
         });
 
-        for (int i = 0; i < jsonValues.size(); i++) {
-            sortedJsonArray.put(jsonValues.get(i));
+        for (JSONObject jsonValue : jsonValues) {
+            sortedJsonArray.put(jsonValue);
         }
+
         return sortedJsonArray;
     }
 }
